1f90f628de364cf7f23567ee04abc2fa781f2134
Mario J. Barchéin Molina <mario@intelligenia.com>
2017-04-05 18:27:35 +0200

    scripts adicionales

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-Proyecto elex
-======================
-
-Proyecto para la gestión de subastas de activos.
-
-Proyecto angular.js + django
 # curso-django-rest-framework-basico
--- update_venv.sh
+++ update_venv.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-command -v virtualenv > /dev/null || {
-	echo "Debe intalar el paquete 'python-virtualenv'"
-	exit 1
-}
+if ! [ -d venv ]; then
+	virtualenv venv
+fi
 
-[ ! -d venv ] && virtualenv venv
-source ./venv/bin/activate
-[ -f src/requirements.txt ] && pip install -r src/requirements.txt || echo "AVISO: No se ha encontrado el fichero src/requirements.txt, no se actualiza el venv"
+./venv/bin/pip install --upgrade pip
+./venv/bin/pip install -r src/requirements.txt
+
