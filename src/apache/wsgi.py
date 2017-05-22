672211e9b3d5d53b79bf7ccd81b6344f8683073d
Mario J. Barchéin Molina <mario@intelligenia.com>
2017-05-22 17:23:28 +0200

    aplicación wsgi

--- src/apache/wsgi.py
+++ src/apache/wsgi.py
@@ -16,7 +16,7 @@ framework.
 
 """
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elex.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myapp.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
